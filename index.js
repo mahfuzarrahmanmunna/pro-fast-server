29653ce770bac9789c9e3b684cd1df5353a313db
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-06-30 13:13:25 +0600

    connection to mongodb and create post to add parcel and get all parchel.

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(process.env.DB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // await client.connect();
+
+        const parcelCollection = client.db('parcelDB').collection('parcels');
+
+        app.get('/all-parcel', async (req, res) => {
+            const result = await parcelCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.post('/add-parcel', async (req, res) => {
+            const newParcel = req.body;
+            const result = await parcelCollection.insertOne(newParcel);
+            res.send(result);
+        });
+
+        console.log("✅ MongoDB connected.");
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Pro fast net is cooking..!');
+});
+
+app.listen(port, () => {
+    console.log(`ProFast app is running on http://localhost:${port}`);
+});
